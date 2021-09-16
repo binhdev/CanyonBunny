6c8ad8132d7fae490a8ee41ae2b8bd782f74a9f1
Thanh Binh <ttbinh@hueic.edu.vn>
2021-09-16 08:35:21 +0700

    first commit

---
+++ desktop/src/com/amzuni/canyon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.amzuni.canyon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.amzuni.canyon.CanyonBunnyMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CanyonBunnyMain(), config);
+	}
+}
